494ecd0be42ce69e2fdd042e6d9fd6c751cb8e37
ezazulhaq.it <ezazulhaq.it@gmail.com>
2025-11-21 01:09:03 +0000

    router included for navigation

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,25 @@
 import { Routes } from '@angular/router';
+import { DocsComponent } from './components/docs/docs';
+import { LoginComponent } from './components/login/login';
+import { authGuard } from './guards/auth';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+    {
+        path: '',
+        redirectTo: '/login',
+        pathMatch: 'full'
+    },
+    {
+        path: 'login',
+        component: LoginComponent
+    },
+    {
+        path: 'docs',
+        component: DocsComponent,
+        canActivate: [authGuard]
+    },
+    {
+        path: '**',
+        redirectTo: '/login'
+    }
+];
--- src/app/app.ts
+++ src/app/app.ts
@@ -1,11 +1,14 @@
 import { Component, signal } from '@angular/core';
+import { RouterOutlet } from '@angular/router';
 
 @Component({
   selector: 'app-root',
-  imports: [],
+  imports: [
+    RouterOutlet
+  ],
   templateUrl: './app.html',
   styleUrl: './app.css'
 })
 export class App {
-  protected readonly title = signal('myapp');
+  protected readonly title = signal('Developer Portal');
 }
